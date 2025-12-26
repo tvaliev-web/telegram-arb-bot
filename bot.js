6eaf49a0a449d1371ed8f2d5af51c7e2faa7991a
tvaliev-web <tvaliev730@gmail.com>
2025-12-26 02:27:58 -0800

    Update bot.js

--- bot.js
+++ bot.js
@@ -1,19 +1,11 @@
 import axios from "axios";
 
-const TG_TOKEN = process.env.TG_TOKEN;
-const TG_CHAT_ID = process.env.TG_CHAT_ID;
+await axios.post(
+  `https://api.telegram.org/bot${process.env.TG_TOKEN}/sendMessage`,
+  {
+    chat_id: process.env.TG_CHAT_ID,
+    text: "BOT STARTED OK"
+  }
+);
 
-async function sendMessage(text) {
-  const url = `https://api.telegram.org/bot${TG_TOKEN}/sendMessage`;
-
-  const res = await axios.post(url, {
-    chat_id: TG_CHAT_ID,
-    text: text
-  });
-
-  console.log("Telegram OK:", res.data);
-}
-
-(async () => {
-  await sendMessage("✅ BOT STARTED SUCCESSFULLY");
-})();
+console.log("DONE");
